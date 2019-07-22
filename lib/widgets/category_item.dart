9f6b5310114b16c80d3a83de8c98e50901c58d29
Anton <anton_real_madrid@icloud.com>
2019-07-23 00:11:08 +0200

    Initial commit

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../screens/category_meals_screen.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final Color color;
+
+  final double borderRadius = 15;
+
+  CategoryItem({
+    @required this.id,
+    @required this.title,
+    @required this.color,
+  });
+
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      CategoryMealsScreen.routeName,
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(borderRadius),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+    );
+  }
+}
